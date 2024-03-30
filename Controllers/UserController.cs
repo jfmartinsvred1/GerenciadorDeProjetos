23980b9862ac3dbe784a51113849ada469174138
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-03-29 21:35:09 -0300

    Initial Configuration

---
+++ Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Gerenciador.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gerenciador.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController:ControllerBase
+    {
+        private IMapper _mapper;
+        private UserManager<IdentityUser> _userManager;
+
+        public UserController(IMapper mapper, UserManager<IdentityUser> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(CreateUserDto dto)
+        {
+            IdentityUser user = _mapper.Map<IdentityUser>(dto);
+            
+            IdentityResult result = await _userManager.CreateAsync(user,dto.Password);
+
+            if (!result.Succeeded)
+            {
+                return Ok("Criado com sucesso");
+            }
+            throw new ApplicationException("Algum Erro");
+        }
+    }
+}
---
+++ Dtos/CreateUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gerenciador.Dtos
+{
+    public class CreateUserDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string Repassword { get; set; }
+    }
+}
